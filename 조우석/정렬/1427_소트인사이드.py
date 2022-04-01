27410123217832f47dda57dc674a7609c8e1d744
useok <55547933+WooseokJ@users.noreply.github.com>
2022-04-01 09:34:38 +0900

    정렬

--- 조우석/정렬/1427_소트인사이드.py
+++ 조우석/정렬/1427_소트인사이드.py
@@ -1,12 +1,28 @@
 import sys
 from typing import MutableSequence
 
-def bubble_sort(a:MutableSequence) -> None:
+# 버블정렬 
+# def bubble_sort(a:MutableSequence) -> None:
+#     n=len(a)
+#     for i in range(n-1):
+#         for j in range(n-1,i,-1):
+#             if a[j-1]<a[j]:
+#                 a[j-1],a[j]=a[j],a[j-1]
+                
+# 삽입정렬
+def insert_sort(a:MutableSequence) -> None:
     n=len(a)
-    for i in range(n-1):
-        for j in range(n-1,i,-1):
-            if a[j-1]>a[j]:
-                a[j-1],a[j]=a[j],a[j-1]
+    for i in range(1,n):
+        j=i
+        tmp=a[i]
+        while j>0 and a[j-1] <tmp:
+            a[j]=a[j-1]
+            j-=1
+        a[j]=tmp
+        
+
 N=list(sys.stdin.readline().strip())
-bubble_sort(N)
-print(N)
+# bubble_sort(N)
+insert_sort(N)
+for i in N:
+    print(i,end='')
