a058ac4a0bb382f42395556fd1c700b21d96ee15
JeonJhin <jdznawa@gmail.com>
2022-04-07 02:10:46 +0900

    0407

--- 전동준/0407_정렬2/1246_온라인판매.py
+++ 전동준/0407_정렬2/1246_온라인판매.py
@@ -1,4 +1,5 @@
-# 직접 작성한 코드 - 채점 결과 틀림(수정 필요)
+# 직접 작성한 코드 - 예시는 맞게 나오는데 채점 결과 틀림
+
 from sys import stdin
 
 n,m = map(int,stdin.readline().split())
@@ -8,18 +9,24 @@ price = []
 
 for z in range(m):
     customer.append(int(stdin.readline()))
-q = len(customer)
+
 # 판매가 오름차순 정렬
+q = len(customer)
 for i in range(q-1):    # 단순 정렬 알고리즘 사용(오름차순)
     lowest = i
     for j in range(i+1, q):
         if customer[j] < customer[lowest]:
             lowest = j
         customer[i], customer[lowest] = customer[lowest], customer[i]
+
 # 판매이익 극대화
 for x in range(1,len(customer)):
-    buy = customer.pop()
-    price.append(buy)
-    income.append((buy * x))
+    if n == 0:
+        break
+    else:
+        wtb = customer.pop()
+        price.append(wtb)
+        income.append((wtb * x))
+        n = n - 1
 
 print(price[income.index(max(income))],max(income))
