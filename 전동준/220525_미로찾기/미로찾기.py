1f777ea97445110cc700af7403eca48ddb777adb
JhinJeon <96213999+JhinJeon@users.noreply.github.com>
2022-05-25 11:21:35 +0900

    미로찾기(152쪽)

---
+++ 전동준/220525_미로찾기/미로찾기.py
@@ -0,0 +1,31 @@
+#bfs 알고리즘 사용
+
+from sys import stdin
+from collections import deque
+
+n,m = map(int,stdin.readline().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int,stdin.readline().strip())))
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def solution(x,y):
+    queue = deque()
+    queue.append(x,y)
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+    return graph[n-1][m-1]
+
+print(solution(0,0))
