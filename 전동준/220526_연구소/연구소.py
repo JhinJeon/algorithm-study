21fa00d61b88d2cd77276a42f5daad1d0d60f7b4
JhinJeon <96213999+JhinJeon@users.noreply.github.com>
2022-05-26 10:12:41 +0900

    Add files via upload

---
+++ 전동준/220526_연구소/연구소.py
@@ -0,0 +1,26 @@
+#DFS
+from sys import stdin
+
+n,m = map(int,stdin.readline().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int,stdin.readline().strip())))
+    
+def solution(x,y):
+    if x <= -1 or x >= m or y <= -1 or y >= n:
+        return False
+    if graph[y][x] == 2:
+        graph[y][x] = 1
+        solution(x-1, y)
+        solution(x, y-1)
+        solution(x+1, y)
+        solution(x, y+1)
+    return False
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 0:
+            result += 1
+            
+print(result)
